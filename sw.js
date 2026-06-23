eb0e8aaaa5b94c861b0c8e1a9f09a06b1c6e6344
Math20000 <mathieuarlabosse@gmail.com>
2026-06-23 09:08:05 +0200

    fix: refresh installed app cache

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'horlojobs-v1';
+const CACHE = 'horlojobs-v2';
 const STATIC_FILES = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(STATIC_FILES)).then(() => self.skipWaiting())));
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('activate', (event) => event.waitUntil(Promise.all([caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))), self.clients.claim()])));
 self.addEventListener('fetch', (event) => {
   const request = event.request;
   if (new URL(request.url).pathname.endsWith('/data/jobs.json')) {
